0e6e258eda37dd0f47a6f187942e7b4f6bd080ed
zawar10 <zawarahmadd123@gmail.com>
2026-06-07 00:00:26 +0500

    Deployment setup-7

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://tourmate-10.netlify.app']
+    origin: ['http://localhost:5173', 'https://tourmate-10.netlify.app/signup', 
+        'https://tourmate-10.netlify.app/signup']
 })); 
 
 // MongoDB Connection
